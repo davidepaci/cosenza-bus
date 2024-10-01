d490f99ae4797e7e75cc1696a21ea3ce1d27910f
Davide Paci <23656588+davidepaci@users.noreply.github.com>
2024-10-01 14:58:57 +0200

    point page showing routes

--- app/app.js
+++ app/app.js
@@ -38,12 +38,12 @@ app.get('/', (req, res) => {
   });
 });
 
-// Route for stop pages
-app.get('/stop/:stopid', (req, res) => {
-  const stopId = req.params.stopid;
+// Route for point pages
+app.get('/fermata/:point', (req, res) => {
+  const pointId = req.params.point;
   const date = new Date().toISOString().slice(0, 10);
   const sql = `
-SELECT DISTINCT r.number, r.terminus
+SELECT DISTINCT r.number, r.terminus, s.time
 FROM route r
 JOIN stop s ON r.number = s.route_id
 WHERE s.point_id = ?  -- Replace with the specific point ID
@@ -51,19 +51,30 @@ ORDER BY s.time;
 `;
 
   // db get all rows
-  db.all(sql, [stopId], (err, rows) => {
+  db.all(sql, [pointId], (err, rows) => {
     if (err) {
       console.error(err.message);
       res.status(500).send('Database error');
       return;
     }
     if (!rows) {
-      res.status(404).send('Stop not found');
+      res.status(404).send('Routes not found');
       return;
     }
-    // respond with json
-    res.json(rows);
-    //res.render('template', { stop: row });
+    // also get the name and id of the point
+    const pointSql = 'SELECT * FROM point WHERE _id = ?';
+    db.get(pointSql, [pointId], (err, row) => {
+      if (err) {
+        console.error(err.message);
+        res.status(500).send('Database error');
+        return;
+      }
+      if (!row) {
+        res.status(404).send('Point not found');
+        return;
+      }
+      res.render('point', { point: row, routes: rows });
+    });
   });
 });
 
